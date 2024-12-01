0e67d3777e747380ed35c829b9a6a91fe058192d
Alex Mohr <mohralexj@gmail.com>
2024-12-01 15:36:11 -0800

    day 1 part 2

--- day01/Main.hs
+++ day01/Main.hs
@@ -7,9 +7,19 @@ main :: IO ()
 main = do
   contents <- readFileFromArgs
   let (xs, ys) = unzip (readPairs contents)
+  part1 (xs, ys)
+  part2 (xs, ys)
+
+part1 :: ([Int], [Int]) -> IO ()
+part1 (xs, ys) = do
   let (xs', ys') = (sort xs, sort ys)
   let totalDistance = sum (zipWith (\x y -> abs (x - y)) xs' ys')
-  print totalDistance
+  putStrLn $ "Part 1: " ++ show totalDistance
+
+part2 :: ([Int], [Int]) -> IO ()
+part2 (xs, ys) = do
+  let total = sum $ map (\x -> x * countOccurrences x ys) xs
+  putStrLn $ "Part 2: " ++ show total
 
 readPairs :: String -> [(Int, Int)]
 readPairs = map readPair . lines
@@ -18,3 +28,6 @@ readPair :: String -> (Int, Int)
 readPair line = case words line of
   [x, y] -> (read x, read y)
   _ -> error "Invalid input"
+
+countOccurrences :: Eq a => a -> [a] -> Int
+countOccurrences x = length . filter (== x)
