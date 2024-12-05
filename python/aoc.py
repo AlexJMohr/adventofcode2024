0be4cb413338e856944a40530ef51ff9fc20da3f
Alex Mohr <alex.mohr@tradablebits.com>
2024-12-05 11:48:29 -0800

    back to python :^)

---
+++ python/aoc.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import re
+
+import click
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+@click.argument('file', type=click.File())
+def day01(file):
+    def parse(file):
+        xs = []
+        ys = []
+        for line in file:
+            x, y = re.search(r'(\d+)\s+(\d+)', line).groups()
+            xs.append(int(x))
+            ys.append(int(y))
+        return xs, ys
+    
+
+    def distance(x, y):
+        return abs(x - y)
+
+    def part1(xs, ys):
+        xs.sort()
+        ys.sort()
+        total = sum(distance(x, y) for x, y in zip(xs, ys))
+        print("Part 1:", total)
+
+    def similarity(x, ys):
+        return x * len([y for y in ys if y == x])
+
+    def part2(xs, ys):
+        total = sum(similarity(x, ys) for x in xs)
+        print("Part 2:", total)
+
+    xs, ys = parse(file)
+    part1(xs, ys)
+    part2(xs, ys)
+
+if __name__ == "__main__":
+    cli()
