fe0f43d0a3eab67650d8751235dc5602584d1353
Alex Mohr <alex.mohr@tradablebits.com>
2024-12-19 11:50:49 -0800

    day 11 part 1

--- python/aoc.py
+++ python/aoc.py
@@ -625,6 +625,28 @@ def day10(file):
     part2(contents)
 
 
+@cli.command()
+@click.argument("file", type=click.File())
+def day11(file):
+    contents = file.read().strip()
+    stones = [int(x) for x in contents.split()]
+
+    for _ in range(25):
+        new_stones = []
+        for stone in stones:
+            if stone == 0:
+                new_stones.append(1)
+            elif len(str(stone)) % 2 == 0:
+                str_stone = str(stone)
+                half = len(str_stone) // 2
+                new_stones.append(int(str_stone[:half]))
+                new_stones.append(int(str_stone[half:]))
+            else:
+                new_stones.append(stone * 2024)
+        stones = new_stones
+
+    print("Part 1:", len(stones)) # 189547
+
 
 if __name__ == "__main__":
     cli()
